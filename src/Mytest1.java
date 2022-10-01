b1cb43d8e8783825eb10a09a744ba9984cfa8d7b
rajesh <rajesh.tummepalli99@gmail.com>
2022-10-01 16:17:08 +0530

    Deleted test2 file

--- src/Mytest1.java
+++ src/Mytest1.java
@@ -3,12 +3,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import net.bytebuddy.implementation.bind.annotation.Super;
 
-public class Mytest1 extends Mytest2 {
+public class Mytest1  {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\M&M\\Myworkspace\\Rajeshnew\\Drivers\\chromedriver.exe");
-		Mytest2 test2 = new Mytest2();
+		
 	
 
 System.out.println("empty methods in test123");
