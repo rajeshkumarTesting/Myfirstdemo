fa819dfce6df01f6209b2fb7030fa3ef89229b3d
rajesh <rajesh.tummepalli99@gmail.com>
2022-10-01 15:57:45 +0530

    changes done to changes removing all and empty methods

--- src/Mytest1.java
+++ src/Mytest1.java
@@ -9,30 +9,13 @@ public class Mytest1 extends Mytest2 {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\M&M\\Myworkspace\\Rajeshnew\\Drivers\\chromedriver.exe");
 		Mytest2 test2 = new Mytest2();
-		
-		WebDriver driver = new ChromeDriver();
-		
-		
-		driver.get("https://www.gmail.com");
-		int a = 3;
-		System.out.println(a);
-			int b= 34;
-System.out.println(b);
-
-int c = a + b;
-
-System.out.println(c);
-
-System.out.println(test2.a);
-System.out.println(test2.b);
-
-System.out.println("Checking the changes in test1");
+	
 
 System.out.println("empty methods in test1");
 
 
 
-test2.newadditions();
+
 	}
 
 }
