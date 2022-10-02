f74d730e287b1310ed05ad53dd17e52df6e9544d
rajesh <rajesh.tummepalli99@gmail.com>
2022-10-02 14:51:21 +0530

    Testng fresh

---
+++ src/Testngintro/Intro1.java
@@ -0,0 +1,27 @@
+package Testngintro;
+
+import org.testng.annotations.Test;
+
+public class Intro1 {
+	
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("I am in intro1 -method1");
+		System.out.println("I am in intro1 -method12");
+	}
+
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("I am in intro1 -method2");
+	}
+	
+	@Test
+	public void method3()
+	{
+		System.out.println("I am in intro1 -method3");
+	}
+}
